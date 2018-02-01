74ebb463ad4d9b4f39031797416f4beaca4cda12
Connor O'Brien <connoro@google.com>
2018-01-31 18:11:57 -0800

    ANDROID: cpufreq: track per-task time in state
    Add time in state data to task structs, and create
    /proc/<pid>/time_in_state files to show how long each individual task
    has run at each frequency.
    Create a CONFIG_CPU_FREQ_TIMES option to enable/disable this tracking.
    
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Bug: 72339335
    Test: Read /proc/<pid>/time_in_state
    Change-Id: Ia6456754f4cb1e83b2bc35efa8fbe9f8696febc8

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -5,6 +5,7 @@
 #include <linux/kernel_stat.h>
 #include <linux/static_key.h>
 #include <linux/context_tracking.h>
+#include <linux/cpufreq_times.h>
 #include "sched.h"
 #include "walt.h"
 
@@ -166,6 +167,10 @@ void account_user_time(struct task_struct *p, cputime_t cputime,
 
 	/* Account for user time used */
 	acct_account_cputime(p);
+#ifdef CONFIG_CPU_FREQ_TIMES
+	/* Account power usage for user time */
+	cpufreq_acct_update_power(p, cputime);
+#endif
 }
 
 /*
@@ -216,6 +221,10 @@ void __account_system_time(struct task_struct *p, cputime_t cputime,
 
 	/* Account for system time used */
 	acct_account_cputime(p);
+#ifdef CONFIG_CPU_FREQ_TIMES
+	/* Account power usage for user time */
+	cpufreq_acct_update_power(p, cputime);
+#endif
 }
 
 /*
